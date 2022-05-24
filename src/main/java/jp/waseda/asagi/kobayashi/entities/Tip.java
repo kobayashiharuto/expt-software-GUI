32124530756bf2dba1d386968cbaba49a6c7db41
kobayashiharuto <datesaver1234@gmail.com>
2022-05-24 22:24:51 +0900

    add tip entity

---
+++ src/main/java/jp/waseda/asagi/kobayashi/entities/Tip.java
@@ -0,0 +1,12 @@
+package jp.waseda.asagi.kobayashi.entities;
+
+public class Tip {
+  public String name;
+  public int amount;
+
+  public Tip(String name, int amount) {
+    this.name = name;
+    this.amount = amount;
+  }
+
+}
